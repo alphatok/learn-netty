ea10061b77a6df0b9050eb192310dc8b287e60f5
bruceyang <yangtzerong@hotmail.com>
2018-10-07 11:03:06 +0800

    add reconnect options

--- netty-demo/src/main/java/com/brzyang/netty/NettyClient.java
+++ netty-demo/src/main/java/com/brzyang/netty/NettyClient.java
@@ -2,32 +2,65 @@ package com.brzyang.netty;
 
 import io.netty.bootstrap.Bootstrap;
 import io.netty.channel.Channel;
+import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.handler.codec.string.StringEncoder;
 
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 public class NettyClient {
+
+    private final static int MAX_RETRY = 10;
     public static void main(String[] args) throws InterruptedException {
         Bootstrap bootstrap = new Bootstrap();
         NioEventLoopGroup group = new NioEventLoopGroup();
 
         bootstrap.group(group)
                 .channel(NioSocketChannel.class)
-                .handler(new ChannelInitializer<Channel>() {
+                .handler(new ChannelInitializer<NioSocketChannel>() {
                     @Override
-                    protected void initChannel(Channel ch) {
+                    protected void initChannel(NioSocketChannel ch) {
                         ch.pipeline().addLast(new StringEncoder());
                     }
                 });
 
-        Channel channel = bootstrap.connect("127.0.0.1", 8000).channel();
+
+        bootstrap
+                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 5000)
+                .option(ChannelOption.SO_KEEPALIVE, true)
+                .option(ChannelOption.TCP_NODELAY, true);
+
+
+        Channel channel = connect(bootstrap, "127.0.0.1", 8000, MAX_RETRY);
 
         while (true) {
             channel.writeAndFlush(new Date() + ": hello world!");
             Thread.sleep(2000);
         }
     }
+
+    private static Channel connect(Bootstrap bootstrap, String host, int port, int retry) {
+        Channel channel = bootstrap.connect(host, port).addListener(future -> {
+            if (future.isSuccess()) {
+                System.out.println("连接成功!");
+            } else if (retry == 0) {
+                System.err.println("重试次数已用完，放弃连接！");
+            } else {
+                // 第几次重连
+                int order = (MAX_RETRY - retry) + 1;
+                // 本次重连的间隔
+                int delay = 1 << order;
+                System.err.println(new Date() + ": 连接失败，第" + order + "次重连……");
+                bootstrap.config().group().schedule(() -> connect(bootstrap, host, port, retry - 1),
+                        delay, TimeUnit.SECONDS);
+            }
+        }).channel();
+
+        return channel;
+    }
+
 }
--- netty-demo/src/main/java/com/brzyang/netty/NettyServer.java
+++ netty-demo/src/main/java/com/brzyang/netty/NettyServer.java
@@ -2,13 +2,14 @@ package com.brzyang.netty;
 
 
 import io.netty.bootstrap.ServerBootstrap;
-import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.ChannelInitializer;
-import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.handler.codec.string.StringDecoder;
+import io.netty.util.AttributeKey;
+import io.netty.util.concurrent.Future;
+import io.netty.util.concurrent.GenericFutureListener;
 
 public class NettyServer {
 
@@ -19,19 +20,52 @@ public class NettyServer {
         NioEventLoopGroup worker = new NioEventLoopGroup();
         serverBootstrap
                 .group(boss, worker)
+                // BIO OioServerSocketChannel
+                // NIO NioServerSocketChannel NioSocketChannel SocketChannel
                 .channel(NioServerSocketChannel.class)
-                .childHandler(new ChannelInitializer<NioSocketChannel>() {
-                    protected void initChannel(NioSocketChannel ch) {
-                        ch.pipeline().addLast(new StringDecoder());
-                        ch.pipeline().addLast(new SimpleChannelInboundHandler<String>() {
-                            @Override
-                            protected void channelRead0(ChannelHandlerContext ctx, String msg) {
-                                System.out.println(msg);
-                            }
-                        });
+                .handler(new ChannelInitializer<NioServerSocketChannel>() {
+                    @Override
+                    protected void initChannel(NioServerSocketChannel ch) throws Exception {
+                        System.out.println("服务端启动中, 通常不需要");
                     }
-                })
-                .bind(8000);
+                });
+        serverBootstrap.attr(AttributeKey.newInstance("serverName"), "nettyServer");
+
+        serverBootstrap.childHandler(new ChannelInitializer<NioSocketChannel>() {
+            protected void initChannel(NioSocketChannel ch) {
+                ch.pipeline().addLast(new StringDecoder());
+                ch.pipeline().addLast(new SimpleChannelInboundHandler<String>() {
+                    @Override
+                    protected void channelRead0(ChannelHandlerContext ctx, String msg) {
+                        System.out.println(msg);
+                    }
+                });
+            }
+        });
+        serverBootstrap.childAttr(AttributeKey.newInstance("clientKey"), "clientValue");
+        serverBootstrap
+                // 开启TCP底层心跳机制
+                .childOption(ChannelOption.SO_KEEPALIVE, true)
+                // 禁用Nagle算法（合并多个TCP包，MSS）
+                .childOption(ChannelOption.TCP_NODELAY, true);
+
+
+        // TODO https://blog.csdn.net/linuu/article/list/3
+        bind(serverBootstrap, 8000);
 
     }
+
+    private static void bind(final ServerBootstrap serverBootstrap, final int port) {
+        serverBootstrap.bind(port).addListener(new GenericFutureListener<Future<? super Void>>() {
+            public void operationComplete(Future<? super Void> future) {
+                if (future.isSuccess()) {
+                    System.out.println("端口[" + port + "]绑定成功!");
+                } else {
+                    System.err.println("端口[" + port + "]绑定失败!");
+                    bind(serverBootstrap, port + 1);
+                }
+            }
+        });
+    }
+
 }
